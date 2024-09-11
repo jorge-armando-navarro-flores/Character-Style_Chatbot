fc3e90837acbd851a1d345af491ab441defdff4b
jorge-armando-navarro-flores <janf.ai.developer@gmail.com>
2024-09-11 12:13:50 -0600

    launch

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ with gr.Blocks() as demo:
     character_dropdown.input(get_character_prompt, inputs=[character_dropdown], outputs=[chatbot])
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
